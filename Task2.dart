6f76fc63aa2d782ba92e04e0da3836eab7996343
Aya Ali <ayaali5202@gmail.com>
2024-10-28 18:59:13 +0300

    uploading new task

--- Task2.dart
+++ Task2.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 // Exercise 1
 // void main() {
@@ -104,8 +105,108 @@ import 'dart:io';
 //     else{
 //       EvenNumberChecker(InputList);
 //     }
-    
+
 //   }
 // }
 
 // Exercise 4
+// int GameFunction(String userInput, dynamic score) {
+//   List<String> playingOptions = <String>["Paper", "Rock", "Scissors"];
+//   int numirecaluserInput = int.parse(userInput);
+
+//   final random = Random();
+//   var computerRandomPlay =
+//       playingOptions[random.nextInt(playingOptions.length)];
+//   print("The computer played $computerRandomPlay.");
+//   print("------------------");
+
+//   if (numirecaluserInput == 1 && computerRandomPlay == "Paper") {
+//     print("You and computer have played the same symbole, so you are equal.\n");
+//     print("------------------");
+//   } else if (numirecaluserInput == 1 && computerRandomPlay == "Scissors") {
+//     print("Hardluck, You lost.\n");
+//     print("------------------");
+//     score = score - 1;
+//   } else if (numirecaluserInput == 1 && computerRandomPlay == "Rock") {
+//     print("Congratulation, You win.\n");
+//     print("------------------");
+//     score = score + 1;
+//   } else if (numirecaluserInput == 2 && computerRandomPlay == "Rock") {
+//     print("You and computer have played the same symbole, so you are equal.\n");
+//     print("------------------");
+//   } else if (numirecaluserInput == 2 && computerRandomPlay == "Scissors") {
+//     print("Congratulation, You win.\n");
+//     score = score + 1;
+//     print("------------------");
+//   } else if (numirecaluserInput == 2 && computerRandomPlay == "Paper") {
+//     print("Hardluck, You lost.\n");
+//     print("------------------");
+//     score = score - 1;
+//   } else if (numirecaluserInput == 3 && computerRandomPlay == "Paper") {
+//     print("Congratulation, You win.\n");
+//     print("------------------");
+//     score = score + 1;
+//   } else if (numirecaluserInput == 3 && computerRandomPlay == "Rock") {
+//     print("Hardluck, You lost.\n");
+//     print("------------------");
+//     score = score - 1;
+//   } else if (numirecaluserInput == 3 && computerRandomPlay == "Scissors") {
+//     print("You and computer have played the same symbole, so you are equal.\n");
+//     print("------------------");
+//   } else {
+//     print(
+//         "We Can't understand your input ,please choose one of provided inputs.\n");
+//   }
+//   if (score == -1) {
+//     score = 0;
+//   }
+//   return score;
+// }
+
+// void main() {
+//   dynamic score = 0;
+//   do {
+//     print("Welcome to Paper Rock and Scissors game.\n");
+//     print(
+//         "If you want to play a symbole please choose the number behind it.\nPaper: 1,\nRock:2,\nScissors:3.\n");
+//     print("------------------");
+//     String userInput = stdin.readLineSync()!;
+//     if (userInput == null) {
+//       print("No input symbole provided please try again.\n");
+//     } else if (userInput.toLowerCase() == 'exsit') {
+//       print("You quit The game.\n");
+//     } else {
+//       var scoreOut = GameFunction(userInput, score);
+//       print("Your score is $scoreOut.");
+//       print("------------------");
+//     }
+//   } while (true);
+// }
+
+// Exercise 5
+List<int> FabonacciProgramm(int number) {
+  List<int> data = [0, 1];
+  for (var i = 2; i < number; i++) {
+    data.add(data[i - 1] + data[i - 2]);
+  }
+  return data;
+}
+
+void main() {
+  do {
+    print("Welcome to Fabonacci Programm.\n");
+    print("Please enter the number of fabonacci numbers you want.\n");
+    String? input = stdin.readLineSync()!;
+    int number = int.parse(input);
+    if (input == null || input.isEmpty) {
+      print("Please enter a vaild  number.");
+      continue;
+    } else if (input == 'exit') {
+      print("Thank you to use out programm.");
+      break;
+    } else {
+      List<int> fabNumbers = FabonacciProgramm(number);
+      print("The Output List is $fabNumbers.");
+    }
+  } while (true);
+}
